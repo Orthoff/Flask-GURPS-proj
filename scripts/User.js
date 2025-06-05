c4dd01218c8cb75913e2b909aa2e37e759d627c1
Orthoff <tlyon1005@gmail.com>
2025-06-04 19:55:11 -0500

    attempt 3, removed flask and all python files to focus on frontend web dev for now

---
+++ scripts/User.js
@@ -0,0 +1,10 @@
+
+const exampleUser = {
+    username: ["Orthoff"],
+    password: ["grunkle"], // need to look into authorization tech stack
+    GURPScharacters: ["here i pass in an array of GURPScharacter objects to be implemented"],
+    createGURPScharacter() {
+        //idk lol
+
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
+console.log(document.title);
+
+const newCharButton = document.getElementById("new-char");
+const viewCharsButton = document.getElementById("my-chars");
+const shareCharsButton = document.getElementById("share-chars");
+ 
+newCharButton.addEventListener("click", )
+
+// attempt at a factory funtion smh
+function charCreator(){
+    return {
+        
+    }
+}
